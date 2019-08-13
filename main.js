02e9bcd08938558b6c615b5f584504e084160392
Hyijun <32429581+Hyijun@users.noreply.github.com>
2019-08-13 20:13:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 s = document.getElementById("mw-content-text").innerHTML;
-s = document.getElementById("mw-content-text").innerHTML = s.replace(/<p>[\\s\S]*?(User:Hyijun)[\s\S]*?<\/p>/gmi, "a");
-s = document.getElementById("mw-content-text").innerHTML = s.replace(/<dd>[\\s\S]*?(User:Hyijun)[\s\S]*?<\/dd>/gmi, "a");
+s = document.getElementById("mw-content-text").innerHTML = s.replace(/<p>[\s\S]*?(User:Hyijun)[\s\S]*?<\/p>/gmi, "a");
+s = document.getElementById("mw-content-text").innerHTML = s.replace(/<dd>[\s\S]*?(User:Hyijun)[\s\S]*?<\/dd>/gmi, "a");
