01ed9b8cc428d9a866e924654f06259c975f524c
Alexander Misel <7542439+AlexanderMisel@users.noreply.github.com>
2019-09-04 16:04:35 +0800

    Fix usage of undeclare variables

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //将你需要屏蔽的用户写在这个数组中，以逗号（半角）分隔，名字用双引号（半角）包围起来，不需要User:前缀
 //例子：var users = new Array("User1", "User2", "User3");
-var users = new Array("Any_users");
+var users = new Array("Any_user");
 
 //如果您不熟悉js语言，请不要修改以下代码
 var url = window.location.href;
@@ -13,7 +13,7 @@ if (!matre.test(url)){
     var res_1 = new Array();
     console.log("屏蔽工具正在运行。");
     var users_name = "";
-    for (each in users){
+    for (var each in users){
         if (each != (users.length - 1)){
             users_name = users_name + users[each] + "|";
         }else{
@@ -21,6 +21,6 @@ if (!matre.test(url)){
         }
     }
     var s1 = document.getElementById("mw-content-text").innerHTML;
-    re1 = new RegExp("<(p|dd|li)>.*?User:(" + users_name +")[\\s\\S]*?</(p|dd|li)>", "igm");
+    var re1 = new RegExp("<(p|dd|li)>.*?User:(" + users_name +")[\\s\\S]*?</(p|dd|li)>", "igm");
     s1 = document.getElementById("mw-content-text").innerHTML = s1.replace(re1, "<small><div>[已删除一段留言]</div></small>");
 }
